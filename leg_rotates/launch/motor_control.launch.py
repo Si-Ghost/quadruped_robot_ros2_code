54f00c0d50a898f6e73a46cd18a18e5defa6bea4
Fy <3283958823@qq.com>
2026-04-29 13:24:13 +0800

    创建了launch文件

---
+++ leg_rotates/launch/motor_control.launch.py
@@ -0,0 +1,23 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    motor_rotates_node = Node(
+        package='leg_rotates',
+        executable='motor_rotates',
+        name='motor_rotates',
+        output='screen',
+    )
+
+    motor_controller_node = Node(
+        package='leg_rotates',
+        executable='motor_controller',
+        name='motor_controller',
+        output='screen',
+    )
+
+    return LaunchDescription([
+        motor_rotates_node,
+        motor_controller_node,
+    ])
